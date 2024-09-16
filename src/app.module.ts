4280e3c3996800689222db871c7d84f7c0370891
Luís Vitor <luis.sg.vitor@gmail.com>
2024-09-16 02:29:08 -0300

    chore: clean project

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { DatabaseModule } from '@/infra/db/db.module';
 import { HttpModule } from '@/infra/http/http.module';
 
 @Module({
   imports: [HttpModule, DatabaseModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
